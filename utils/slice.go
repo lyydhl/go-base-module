7153026edfe8d5003e60b2c006122e5637112856
Bonjour <1185230223@qq.com>
2020-11-10 17:56:40 +0800

    slice 去重方法

---
+++ utils/slice.go
@@ -0,0 +1,14 @@
+package utils
+
+// Slice去重
+func RemoveDuplicateElement(items []string) []string {
+	result := make([]string, 0, len(items))
+	temp := map[string]struct{}{}
+	for _, item := range items {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
